d874ac7442c00b38eca18ec077b239eafa2fc6f9
DGHX12345 <2595361514@qq.com>
2026-03-26 18:40:37 +0800

    验证 Adora recv_async() 异步 API

---
+++ adora-python-async/send_data.py
@@ -0,0 +1,18 @@
+import time
+
+import numpy as np
+import pyarrow as pa
+from adora import Node
+
+node = Node()
+
+i = 0
+for event in node:
+    if event["type"] == "INPUT":
+        if i == 100:
+            break
+        i += 1
+        now = time.perf_counter_ns()
+        node.send_output("data", pa.array([np.uint64(now)]))
+    elif event["type"] == "STOP":
+        break
